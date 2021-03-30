9963ee0f732903c7bdaceb4df535c096afa2da2a
Lenovo <strezevar@gmail.com>
2021-03-30 10:57:52 +0300

    2

--- game.py
+++ game.py
@@ -2,32 +2,70 @@ import pygame as pg
 
 pg.init()
 
-win = pg.display.set_mode((500, 500))
+win = pg.display.set_mode((512, 512))
 
 pg.display.set_caption('Plane')
 
-x = 450
-y = 430
+walkRight = [pg.image.load('1.png'), pg.image.load('2.png')]
+walkLeft = [pg.image.load('-1.png'), pg.image.load('-2.png')]
+planeStand = pg.image.load('0.png')
+bg = pg.image.load('bg.bmp')
+
+clock = pg.time.Clock()
+
 width = 40
 height = 60
-speed = 15
+speed = 3
+x = 250-width/2
+y = 430
+
+left = False
+right = False
+animCount = 0
+
+
+def drawWindow():
+    global animCount
+    win.blit(bg, (0, 0))
+
+    if animCount + 1 >= 30:
+        animCount = 0
+
+    if left:
+        win.blit(walkLeft[animCount // 15], (x, y))
+        animCount += 1
+    elif right:
+        win.blit(walkRight[animCount // 15], (x, y))
+        animCount += 1
+    else:
+        win.blit(planeStand, (x, y))
+
+    pg.display.update()
+
 
 run = True
 while run:
-    pg.time.delay(100)
+    clock.tick(30)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
 
     keys = pg.key.get_pressed()
-    if keys[pg.K_LEFT]:
+    if keys[pg.K_LEFT] and x > 20:
         x -= speed
-    if keys[pg.K_RIGHT]:
+        left = True
+        right = False
+    elif keys[pg.K_RIGHT] and x < 500-width-20:
         x += speed
+        left = False
+        right = True
+    else:
+        left = False
+        right = False
+        animCount = 0
 
-    win.fill((0, 0, 0))
-    pg.draw.rect(win, (0, 0, 255), (x, y, width, height))
-    pg.display.update()
+    drawWindow()
+
 
 pg.quit()
