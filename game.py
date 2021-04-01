e7f72f6ec1efce887d49a53f3e0e47094513030a
Lenovo <strezevar@gmail.com>
2021-04-01 11:50:58 +0300

    4

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import random
 import pygame as pg
 
 pg.init()
@@ -6,7 +7,7 @@ win = pg.display.set_mode((512, 512))
 
 pg.display.set_caption('Plane')
 
-bg = pg.image.load('bg.bmp')
+bg = pg.image.load('img/bg.bmp')
 
 clock = pg.time.Clock()
 
@@ -17,11 +18,24 @@ x = 250 - width / 2
 y = 430
 y_shot = y
 
+bullets = []
+enemies = []
+rate = 0
+planeRate = 0
+enemySpeed = 3
+
 left = False
 right = False
 isShot = False
 animCount = 0
 
+class enemyPlane():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self, win):
+        win.blit(pg.image.load('img/enemy.png'), (self.x, self.y))
 
 class attack():
     def __init__(self, x, y, radius, color):
@@ -44,23 +58,24 @@ def drawWindow():
         animCount = 0
 
     if left:
-        win.blit(pg.image.load('-.png'), (x, y))
+        win.blit(pg.image.load('img/-.png'), (x, y))
         animCount += 1
     elif right:
-        win.blit(pg.image.load('+.png'), (x, y))
+        win.blit(pg.image.load('img/+.png'), (x, y))
         animCount += 1
     else:
-        win.blit(pg.image.load('0.png'), (x, y))
+        win.blit(pg.image.load('img/0.png'), (x, y))
 
     for bullet in bullets:
         bullet.draw(win)
 
+    for enemy in enemies:
+        enemy.draw(win)
+
     pg.display.update()
 
-
-bullets = []
-rate = 0
 run = True
+
 while run:
     clock.tick(30)
 
@@ -93,11 +108,24 @@ while run:
             bullets.pop(bullets.index(bullet))
 
     if keys[pg.K_SPACE]:
-        if len(bullets) < 5 and rate % 15 == 0:
+        if len(bullets) < 5 and rate % 5 == 0:
             bullets.append(attack(round(x + 35), round(y + 20), 5, (255, 0, 0)))
         rate += 1
-        if rate > 90:
+        if rate > 6:
             rate = 1
+
+    for enemy in enemies:
+        if enemy.y < 420:
+            enemy.y += enemySpeed
+        else:
+            enemies.pop(enemies.index(enemy))
+
+    if len(enemies) < 3 and round(planeRate) % 8 == 0:
+        enemies.append(enemyPlane(random.randint(20, 420), -55))
+    planeRate += 1
+
+    if planeRate > 8:
+        planeRate = 1
 
     drawWindow()
 
